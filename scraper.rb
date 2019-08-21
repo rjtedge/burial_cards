57cf1a6501b35a7ab2a8d97a489949946a90c631
Rob Tedge <rjtedge@users.noreply.github.com>
2019-08-21 15:45:09 +1000

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -11,18 +11,19 @@ while pagenum <= 27854
      p pagenum
   
   page = agent.get(urlbase + pagenum.to_s)
-  burialdate = page.at('.fieldvalue_begraafdatum').inner_text.strip
-  archivecardnum = page.at('.fieldvalue_archiefkaartnummer').inner_text.strip
-  familyfnameone = page.at('.fieldvalue_familienaam_1').inner_text.strip
-  familyfnametwo = page.at('.fieldvalue_familienaam_2').inner_text.strip
-  firstname = page.at('.fieldvalue_voornaam').inner_text.strip
-  location = page.at('.fieldvalue_positie').inner_text.strip
-  field = page.at('.fieldvalue_veld').inner_text.strip
-  row = page.at('.fieldvalue_rij').inner_text.strip
-  grave = page.at('.fieldvalue_graf').inner_text.strip
+  page.at('.fieldvalue_begraafdatum') == nil ? (burialdate = "") : (burialdate = page.at('.fieldvalue_begraafdatum').inner_text.strip)
+  page.at('.fieldvalue_archiefkaartnummer') == nil ? (archivecardnum = "") : (archivecardnum = page.at('.fieldvalue_archiefkaartnummer').inner_text.strip)
+  page.at('.fieldvalue_familienaam_1') == nil ? (familyfnameone = "") : (familyfnameone = page.at('.fieldvalue_familienaam_1').inner_text.strip)
+  page.at('.fieldvalue_familienaam_2') == nil ? (familyfnametwo = "") : (familyfnametwo = page.at('.fieldvalue_familienaam_2').inner_text.strip)
+  page.at('.fieldvalue_voornaam') == nil ? (firstname = "") : (firstname = page.at('.fieldvalue_voornaam').inner_text.strip)
+  page.at('.fieldvalue_positie') == nil ? (location = "") : (location = page.at('.fieldvalue_positie').inner_text.strip)
+  page.at('.fieldvalue_veld') == nil ? (field = "") : (field = page.at('.fieldvalue_veld').inner_text.strip)
+  page.at('.fieldvalue_rij') == nil ? (row = "") : (row = page.at('.fieldvalue_rij').inner_text.strip)
+  page.at('.fieldvalue_graf') == nil ? (grave = "") : (grave = page.at('.fieldvalue_graf').inner_text.strip)
+
   gravestonephotourl = ""
   if page.at('.fieldvalue_foto_grafsteen_aanwezig img').values[0] == "images/check_yes.gif"
-    gravestonephotourl = page.at('.fieldvalue_grafsteen_1 img').attributes['src'].value
+    p gravestonephotourl = page.at('.fieldvalue_grafsteen_1 img').attributes['src'].value
   end
 
    
