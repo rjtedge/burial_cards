0afc33c21c56bf65c6b96ab6cd04894b488f31ae
Rob Tedge <rjtedge@users.noreply.github.com>
2019-08-21 15:52:31 +1000

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -24,7 +24,7 @@ while pagenum <= 27854
     gravestonephotourl = ""
   if page.at('.fieldvalue_foto_grafsteen_aanwezig img') == nil 
     gravestonephotourl = ""
-  else page.at('.fieldvalue_foto_grafsteen_aanwezig img').values[0] == "images/check_yes.gif"
+  elsif page.at('.fieldvalue_foto_grafsteen_aanwezig img').values[0] == "images/check_yes.gif"
     gravestonephotourl = page.at('.fieldvalue_grafsteen_1 img').attributes['src'].value
   end
 
